d61cb981e3e48b3b0032ee870752e8de69367173
sinjwu <harbirdouk@gmail.com>
2025-06-19 19:21:30 +0900

    250619 babylion callback

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+    interface OnClickListener { //인터페이스를 만들고 구현체를 통해 실행
+        void onClick();
+    }
+    private OnClickListener listener; //구현체를 사용해 실제로 저장할 필드
+
+    public void setOnClickListener(OnClickListener listener) { //listener를 등록하는 setter
+        this.listener = listener;
+    }
+    public void click() { //클릭했을 때 listener가 설정되어 있으면 동작
+        if (listener != null) {
+            listener.onClick();
+        }
+    }
+    //내부 클래스: 외부와 강하게 결합되지 않음
+    private class ClickHandler implements OnClickListener { //버튼 클릭 시 실제 수행할 로직
+        public void onClick() {
+            System.out.println("Button was clicked!");
+        }
+    }
+    public void simulate() { //테스트
+        setOnClickListener(new ClickHandler());
+        click();
+    }
+    public static void main(String[] args) {
+        Main btn = new Main();
+        btn.simulate();
+    }
+}
